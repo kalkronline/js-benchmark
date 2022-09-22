8dde1e33523fd9c85c236426b39f110a75811403
Kale Riley <kalkronline@gmail.com>
2022-09-21 18:08:51 -0700

    add err handling

--- src/app/Test.svelte
+++ src/app/Test.svelte
@@ -17,22 +17,34 @@
 
         slice.result = res;
     }
+
+    function log_err(e) {
+        console.error(e);
+        return '';
+    }
 </script>
 
 {#if slice.result}
     <tr class:sel={id == selected} on:click={() => selected = id}>
-        <td width="auto" style='white-space: nowrap'>Test {id + 1}</td>
+        <td width="auto" style='white-space: nowrap'>Benchmark {id + 1}</td>
         <td width="100%"></td>
         <td width="auto" style='text-align: right'>{slice.result}</td>
     </tr>
 {:else}
 {#await test(slice.run)}
     <tr>
-        <td width="auto" style='white-space: nowrap'>Running Test...</td>
+        <td width="auto" style='white-space: nowrap'>Running Benchmark...</td>
         <td width="100%"></td>
     </tr>
 {:then val} 
     {set_result(val)}
+{:catch e}
+    <tr class:sel={id == selected} on:click={() => selected = id}>
+        {log_err(e)}
+        <td width="auto" style='white-space: nowrap'>Benchmark {id+1} Failed</td>
+        <td width="100%"></td>
+        <td width="auto"></td>
+    </tr>
 {/await}
 {/if}
 
